c5b0b02998162e827b73c0fabef3e697863840a0
RaduMilici <Radu.Milici.3pg@phishme.com>
2018-06-16 15:34:34 +0300

    deleted debug tools

--- src/const/colors.ts
+++
@@ -1,6 +0,0 @@
-const start: string = 'red';
-const obstacle: string = 'black';
-const outline: string = 'black';
-const empty: string = 'white';
-
-export { start, obstacle, outline, empty };
--- src/interfaces/update.ts
+++
@@ -1,6 +0,0 @@
-import id from './id';
-import Time from '../util/Time';
-
-export default interface update extends id {
-  update(time: Time): void;
-}
