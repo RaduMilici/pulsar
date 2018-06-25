502558cd68135afd690d986fbce5c8673b7fd5b1
RaduMilici <radu.milici@3pillarglobal.com>
2018-06-25 23:26:33 +0300

    d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,9 @@
 import Grid from './pathfinding/Grid';
 import Navigator from './pathfinding/Navigator';
 import NavigatorTile from './pathfinding/NavigatorTile';
-import pathfinding from './pathfinding';
 import Triangulation from './triangulation/Triangulation';
 import Vector from './triangulation/Vector';
 import Line from './triangulation/Line';
 import Triangle from './triangulation/Triangle';
-export { pathfinding, Grid, Navigator, NavigatorTile, Triangulation, Vector, Line, Triangle };
+export { Grid, Navigator, NavigatorTile, Triangulation, Vector, Line, Triangle };
 //# sourceMappingURL=index.d.ts.map
