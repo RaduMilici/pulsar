694be6951079d399e7a99037adf8452b17e5f20b
RaduMilici <Radu.Milici.3pg@3pillarglobal.com.com>
2019-10-03 22:42:04 +0300

    uniqueId specs

--- specs/util/uniqueID.spec.ts
+++ specs/util/uniqueID.spec.ts
@@ -2,16 +2,14 @@ import { uniqueId } from '../../src/util';
 
 describe('until / uniqueId', () => {
   it('should generate an incrementing unique id', () => {
-    const count: number = 50;
-    const ids: number[] = [];
+    const count: number = 100000;
+    const ids: string[] = [];
 
     for (let i = 0; i < count; i++) {
       ids.push(uniqueId());
     }
 
-    for (let i = 1; i < count - 1; i++) {
-      expect(ids[i]).toBeLessThan(ids[i + 1]);
-      expect(ids[i]).toBeGreaterThan(ids[i - 1]);
-    }
+    const set: Set<string> = new Set(ids);
+    expect([...set].length).toBe(ids.length);
   });
 });
