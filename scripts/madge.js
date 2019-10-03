9dba716451cb2a82d7d4deb8c9452ebe15b2b8c8
RaduMilici <Radu.Milici.3pg@3pillarglobal.com.com>
2019-10-03 23:25:44 +0300

    format

--- scripts/madge.js
+++ scripts/madge.js
@@ -3,6 +3,6 @@ const madge = require('madge');
 const entryPointPath = path.resolve(`${__dirname}/../src/index.ts`);
 const tsConfig = path.resolve(`${__dirname}/../tsconfig.json`);
 const picturePath = path.resolve(`${__dirname}/dependencyTree.svg`);
-
 const config = { fileExtensions: ['ts'], tsConfig };
+
 madge(entryPointPath, config).then(res => res.image(picturePath));
