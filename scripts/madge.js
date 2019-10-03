1aaad48980e163705b1fc075c31197a2fa578733
RaduMilici <Radu.Milici.3pg@3pillarglobal.com.com>
2019-10-03 23:24:48 +0300

    format

--- scripts/madge.js
+++ scripts/madge.js
@@ -6,9 +6,4 @@ const picturePath = path.resolve(`${__dirname}/dependencyTree.svg`);
 console.log(tsConfig);
 
 const config = { fileExtensions: ['ts'], tsConfig };
-
-madge(entryPointPath, config)
-	.then(res => res.image(picturePath))
-	.then(writtenImagePath => {
-		console.log('Image written to ' + writtenImagePath);
-	});
+madge(entryPointPath, config).then(res => res.image(picturePath));
