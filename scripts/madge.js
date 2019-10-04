9662a9e6d942d9076ab1a2d740add48892462af2
RaduMilici <Radu.Milici.3pg@3pillarglobal.com.com>
2019-10-05 00:26:32 +0300

    madge-circular

--- scripts/madge.js
+++ scripts/madge.js
@@ -2,7 +2,13 @@ const path = require('path');
 const madge = require('madge');
 const entryPointPath = path.resolve(`${__dirname}/../src/index.ts`);
 const tsConfig = path.resolve(`${__dirname}/../tsconfig.json`);
-const picturePath = path.resolve(`${__dirname}/dependencyTree.svg`);
-const config = { fileExtensions: ['ts'], tsConfig };
+const picturePath = path.resolve(`${__dirname}/dependencyTree.png`);
+const config = { 
+  fileExtensions: ['ts'], 
+  tsConfig,
+  cyclicNodeColor: 'red'
+};
 
-madge(entryPointPath, config).then(res => res.image(picturePath));
+madge(entryPointPath, config).then(res => {
+  res.image(picturePath);
+});
