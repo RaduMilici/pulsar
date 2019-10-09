6e52a18140e8b4820c25ef0592dc746a2e6adaf0
RaduMilici <Radu.Milici.3pg@3pillarglobal.com.com>
2019-10-09 18:29:58 +0300

    build script

--- scripts/build-debugger/index.js
+++ scripts/build-debugger/index.js
@@ -3,18 +3,21 @@ const webpack = require('webpack');
 const chalk = require('chalk');
 const { config, debuggerRoot } = require('./webpack.config');
 
-
 const onBuildComplete = () => {
-  const src = `${debuggerRoot}/src/indesx.html`;
-  const dest = `${debuggerRoot}/build/index.html`;
   try {
-    fs.copyFileSync(src, dest);
+    copyIndexToDist();
     console.log(chalk.green.bold('*** successfully built Pulsar debugger ***'));    
   }
   catch (err) {
     console.error(chalk.red.bold('*** error building Pulsar debugger ***')); 
     console.error(err);    
   }  
+}
+
+const copyIndexToDist = () => {
+  const src = `${debuggerRoot}/src/index.html`;
+  const dest = `${debuggerRoot}/build/index.html`;
+  fs.copyFileSync(src, dest);
 }
 
 webpack(config).run(onBuildComplete);
