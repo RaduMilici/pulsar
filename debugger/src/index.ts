9611e67d5b60422a6caf72bba916bad73e0f0644
RaduMilici <Radu.Milici.3pg@3pillarglobal.com.com>
2019-10-09 18:36:20 +0300

    html containers

--- debugger/src/index.ts
+++ debugger/src/index.ts
@@ -1,6 +1,6 @@
 import Editor from './Editor';
 import * as pulsar from '../../src';
-const container: HTMLElement = document.getElementById('container');
+const editorContainer: HTMLElement = document.getElementById('editor-container');
 const code = 
 `const { Vector } = pulsar;
 const a = new Vector({ x: 3, y: 7 });
@@ -8,4 +8,4 @@ const b = new Vector({ x: 5, y: 1 });
 const magnitude: number = a.add(b).magnitude();
 console.log(a, b, magnitude);
 `;
-new Editor(container, code, [pulsar]);
+new Editor(editorContainer, code, [pulsar]);
