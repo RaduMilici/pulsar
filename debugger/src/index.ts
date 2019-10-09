6c2bcb1c207421d29833091c0eae3400d0e546e8
RaduMilici <Radu.Milici.3pg@3pillarglobal.com.com>
2019-10-09 14:09:28 +0300

    onDidChangeModelContent

--- debugger/src/index.ts
+++ debugger/src/index.ts
@@ -1,15 +1,32 @@
 import * as monaco from 'monaco-editor';
-const a: number = 3;
+import * as pulsar from '../../src';
 const container: HTMLElement = document.getElementById('container');
-const code = `
-function x(): void {
-  console.log('hello world!');
-}
-
-x();
+const code = 
+`const { Vector } = pulsar;
+console.log(pulsar);
+const a = new Vector({ x: 3, y: 7 });
+const b = new Vector({ x: 5, y: 1 });
+const magnitude: number = a.add(b).magnitude();
+console.log(a, b, magnitude);
 `;
 
-monaco.editor.create(container, {
+const editor = monaco.editor.create(container, {
   value: code,
-  language: 'typescript'
+  language: 'typescript',
+  fontSize: 16,
 });
+monaco.editor.setTheme('vs-dark');
+monaco.languages.typescript.typescriptDefaults.addExtraLib('declare var pulsar: any;');
+
+const compile = async (editor: any) => {
+  const woker: any = await monaco.languages.typescript.getTypeScriptWorker();
+  const proxy: any = await woker(editor.getModel().uri);
+  const result: any = await proxy.getEmitOutput(editor.getModel().uri.toString());
+  const text: string = result.outputFiles[0].text;
+  const f: Function = new Function('pulsar', text);
+  f.apply(null, [pulsar]);
+}
+
+editor.onDidChangeModelContent((event) => { compile(editor); });
+
+compile(editor);
