0af26e95a88d76f7f2742be4081e459f0c21746d
RaduMilici <Radu.Milici.3pg@3pillarglobal.com.com>
2019-10-11 14:07:59 +0300

    ast

--- scripts/build-debugger/ast.js
+++ scripts/build-debugger/ast.js
@@ -1,27 +1,33 @@
 const path = require('path');
 const { Project } = require('ts-morph');
 
-const srcPath = path.resolve(__dirname, '../../src');
-const project = new Project({ 
-  compilerOptions: { 
-    outDir: 'scripts/build-debugger/build', 
-  },
-});
+const generateDTSfiles = () => {
+  const srcPath = path.resolve(__dirname, '../../src');
+  const project = new Project({ 
+    compilerOptions: { 
+      outDir: 'scripts/build-debugger/dtsBuild', 
+    },
+  });
+  
+  project.addExistingSourceFiles(`${srcPath}/**/*.ts`);
+  const sourceFiles = project.getSourceFiles();
+  
+  sourceFiles.forEach(file => {
+    if (file.isDeclarationFile()) {
+      return;
+    }
+  
+    const importStatements = file.getImportDeclarations();
+    const exportStatements = file.getExportDeclarations();
+  
+    importStatements.forEach(statement => statement.remove());
+    exportStatements.forEach(statement => statement.remove());
+    file.removeDefaultExport();
+  });
+  
+  project.emitSync({ emitOnlyDtsFiles: true });
+}
 
-project.addExistingSourceFiles(`${srcPath}/**/*.ts`);
-const sourceFiles = project.getSourceFiles();
 
-sourceFiles.forEach(file => {
-  if (file.isDeclarationFile()) {
-    return;
-  }
 
-  const importStatements = file.getImportDeclarations();
-  const exportStatements = file.getExportDeclarations();
-
-  importStatements.forEach(statement => statement.remove());
-  exportStatements.forEach(statement => statement.remove());
-  file.removeDefaultExport();
-});
-
-project.emitSync({ emitOnlyDtsFiles: true });
+generateDTSfiles();
