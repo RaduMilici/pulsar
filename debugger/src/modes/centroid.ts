fe2ade6f0b8377a84c7d73ba0f7541bdfe0b465c
RaduMilici <Radu.Milici.3pg@3pillarglobal.com.com>
2019-10-15 00:15:40 +0300

    centroid triangle

--- debugger/src/modes/centroid.ts
+++ debugger/src/modes/centroid.ts
@@ -4,7 +4,7 @@ const bottomRight = new Pulsar.Vector({ x: 300, y: -300 });
 const bottomLeft = new Pulsar.Vector({ x: -300, y: -300 });
 const boxPoints: Pulsar.Vector[] = [topLeft, topRight, bottomRight, bottomLeft];
 const box: Pulsar.BoundingBox = new Pulsar.BoundingBox(boxPoints);
-const randomPoints: Pulsar.Vector[] = Pulsar.randomPoints(50, box);
+const randomPoints: Pulsar.Vector[] = Pulsar.randomPoints(3, box);
 const { hull }: Pulsar.Triangulation = new Pulsar.Triangulation(randomPoints);
 hull.start();
 draw.lines(hull.lines);
