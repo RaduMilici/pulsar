af1affd221024cf4c627d1a37016bf9012913689
RaduMilici <radu.milici@3pillarglobal.com>
2020-03-28 00:48:18 +0200

    format

--- specs/ecs/fixtures/SpecGameObject.ts
+++ specs/ecs/fixtures/SpecGameObject.ts
@@ -1,7 +1,7 @@
 import { GameObject } from '../../../src/ecs';
 
 export default class SpecGameObject extends GameObject {
-    constructor() {
-        super({ name: 'SpecGameObject' });
-    }
+  constructor() {
+    super({ name: 'SpecGameObject' });
+  }
 }
