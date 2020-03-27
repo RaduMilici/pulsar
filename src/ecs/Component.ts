a95d7cfa43185d2efff77dcc738ae48265c2c627
RaduMilici <radu.milici@3pillarglobal.com>
2020-03-27 22:39:47 +0200

    added updatePriority to Component

--- src/ecs/Component.ts
+++ src/ecs/Component.ts
@@ -4,9 +4,11 @@ import Entity from './Entity';
 
 export default abstract class Component extends Entity implements I_Component {
   public parent: I_GameObject;
+  readonly updatePriority: number;
 
   constructor({ name, updatePriority = null }: componentSettings) {
     super({ name });
+    this.updatePriority = updatePriority;
   }
 
   update(tickData: tickData): void {}
--- src/interfaces/ECS/I_Component.ts
+++ src/interfaces/ECS/I_Component.ts
@@ -3,4 +3,5 @@ import I_Updatable from './I_Updatable';
 
 export default interface I_Component extends I_Entity, I_Updatable {
   parent: I_Entity;
+  updatePriority: number;
 }
