3e9093028f66f1809d72c4cc8417532fe265c249
RaduMilici <radu.milici@3pillarglobal.com>
2020-03-28 02:34:52 +0200

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
+    transform: {
+        '^.+\\.tsx?$': "ts-jest"
       },
-      "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-      "moduleFileExtensions": [
+      testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+      moduleFileExtensions: [
         "ts",
         "tsx",
         "js",
