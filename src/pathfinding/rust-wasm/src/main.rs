eb6224fe7b26d886e18eb013da098cdd10c3068e
RaduMilici <radu.milici@3pillarglobal.com>
2020-11-18 18:18:42 +0200

    - error handling - removed rand

--- src/pathfinding/rust-wasm/src/main.rs
+++ src/pathfinding/rust-wasm/src/main.rs
@@ -1,12 +1,21 @@
-use std::fs::File;
+use std::fs::{read_to_string, File};
 use std::io::{Error, Read};
 
 fn main() {
-    let file_content = read_file("src/hello.txt").expect("could not read file");
-    println!("file_content:\n{}", file_content);
+    let path = "src/hello.txt";
+    let file_content = read_file(path).expect("could not read file");
+    println!("*file_content:\n{}", file_content);
+
+    let file_content =
+        read_file_with_error_propagation(path).expect("could not read file");
+    println!("*file_content from error propagation:\n{}", file_content);
+
+    let file_content =
+        read_file_with_read_to_string(path).expect("could not read file");
+    println!("*file_content from read_to_string:\n{}", file_content);
 }
 
-fn read_file(path: &str) -> Result<String, Error> {
+pub fn read_file(path: &str) -> Result<String, Error> {
     let file = File::open(path);
 
     let mut file = match file {
@@ -20,4 +29,14 @@ fn read_file(path: &str) -> Result<String, Error> {
         Ok(_) => Ok(content),
         Err(e) => Err(e),
     }
+}
+
+fn read_file_with_error_propagation(path: &str) -> Result<String, Error> {
+    let mut content = String::new();
+    File::open(path)?.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn read_file_with_read_to_string(path: &str) -> Result<String, Error> {
+    read_to_string(path)
 }
